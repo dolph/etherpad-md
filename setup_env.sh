ed5fef4122f697cea1f04ca03cd1ab80aa2ec987
Dolph Mathews <dolph.mathews@gmail.com>
2015-01-19 16:15:52 +0000

    full environment setup for debian 7.8

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,11 @@
-# node for etherpad dev
-export PATH=$HOME/local/node/bin:$PATH
-export NODE_PATH=$HOME/local/node:$HOME/local/node/lib/node_modules
+# setup node.js for etherpad development
+sudo apt-get update
+sudo apt-get install curl -V -y
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install build-essential -V -y # required for npm
+sudo apt-get install nodejs -V -y
+sudo apt-get install mysql-server -V -y
+sh bin/installDeps.sh
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+# export PATH=$HOME/local/node/bin:$PATH
+# export NODE_PATH=$HOME/local/node:$HOME/local/node/lib/node_modules
